2ce021e67bfe2e4847b60419551262c088e6fe27
Eduardo Shinkawa <eshinkawa@gmail.com>
2024-01-30 00:14:31 -0300

    commit inicial com boilerplate

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { Link, Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  //create a stack navigator
+
+  return (
+    <View style={styles.container}>
+      <Link href="/login">
+        <Text>Hello</Text>
+      </Link>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
